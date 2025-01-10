0be4d90fd9e4212b3c406a08e46db2e075bd4efd
Narangerel0121 <nrngrl0121@gmail.com>
2025-01-10 14:06:52 +0800

    a

---
+++ index.js
@@ -0,0 +1,527 @@
+
+// var age = 14
+// if ( 16 <= age ) { console.log('irgenii unemlehteishdeee')} 
+// else {console.log('unemlehguishdee')} 
+
+// var x=26, y=3;
+// var z = x * y;
+// var w = x % 5;
+// var e = y % 1;
+
+// var day = 87;
+// if(day >= 1 && day <= 9) {
+//     console.log('ehnii 9')
+// }
+// else if(day >= 10 && day <= 18) {
+//     console.log('hoyrdahi 9')
+// }
+// else if(day >= 19 && day <= 27) {
+//     console.log('guravdahi 9')
+// }
+// else if(day >= 28 && day <= 36) {
+//     console.log('dorovdeh 9')
+// }
+// else if(day >= 37 && day <= 45) {
+//     console.log('tavdahi 9')
+// }
+// else if(day >= 46 && day <= 54) {
+//     console.log('zurgaadahi 9')
+// }
+// else if(day >= 55 && day <= 63) {
+//     console.log('doloodahi 9')
+// }
+// else if(day >= 64 && day <= 72) {
+//     console.log('naimdahi 9')
+// }
+// else if(day >= 73 && day <= 81) {
+//     console.log('esdeh 9')
+// }
+// else if(day >= 82) {
+//     console.log('dulaahanshdee')
+// }
+
+
+// var text = ''
+// for(var i = 0 ; i < 5; i++) {
+//     text += '*'
+//     console.log(text)
+// }
+
+// for(var i = 1; i <= 100; i++) {
+//    if( i%2===1){console.log(i) ;}   
+// }
+
+// var num = 0
+// for(var i = 0 ; i < 101; i++) {
+//    num = num + i 
+//     }
+//    console.log(num)
+
+// var num = 1
+// for(var i = 1 ; i<= 100; i++ ) {
+//    if (i % 2 === 0) (num = num * i) 
+//     } console.log(num);
+
+// for(var i = 1; i >= 100; i++) {
+//    if (i%2 != 0){console.log(i);}
+// }
+
+//   var sum = 0
+//   for(var i = 1; i <= 100; i++){
+//      if ( i%2 == 0)
+//   {sum = sum + i}
+//   }
+//   console.log(sum)
+
+//   var mul = 1
+//   for(var i=1; i <= 100; i++){
+//      if (i%2 == 0)
+//        {mul = mul * i}
+//   }
+//   console.log(mul);
+
+//  var text = 'hasah'
+//  var reverse = ''
+//  for( i = text.length - 1; i >= 0; i--) {
+//  reverse = reverse + text[i]
+//  } if(text == reverse) {
+//    console.log(true)
+//  } else{
+//    console.log(false)
+//  }
+
+// var too = 3;
+// var zereg = 6;
+// console.log(too **= zereg)
+
+// var too = 3;
+// var zereg = 6;
+// var exponent = too ** zereg;
+// console.log(exponent)
+
+//   var too = 3;
+//   var zereg = 6;
+//   var exponent = 1
+//   for( var i = 1; i <= zereg; i++){
+//      exponent = exponent * too
+//   } 
+//      console.log(exponent)
+
+// function isPrimeNumber (n){
+//   let isPrimeNumber = true;
+//   if (n == 1) isPrimeNumber = false;
+// for(let i = 2; i < n; i++){
+//   if( n%i == 0){ 
+//     isPrimeNumber = false
+//   }
+// }
+// return isPrimeNumber
+// }
+// console.log(isPrimeNumber(9))
+
+// var x = 13;
+// var result;
+// for(var i = 2; i < x; i++){
+//   result = x % i
+// if (result == 0){
+//   console.log(false)
+// }
+// else{
+//   console.log(true)
+// }
+// }
+
+
+// function isPrimeNumber (n){
+//   let isPrimeNumber = true;
+//   if (n == 1) isPrimeNumber = false;
+// for(let i = 2; i < n; i++){
+//   if( n%i == 0){ 
+//     isPrimeNumber = false
+//   }
+// }
+// return isPrimeNumber
+// }
+// console.log(isPrimeNumber(n=0; n>=100; n++))
+
+
+
+// function isPrimeNumber (n){
+//   let isPrimeNumber = true;
+//   if (n == 1) isPrimeNumber = false;
+//   for(n=0; n>=100; n++){
+// for(let i = 2; i < n; i++){
+//   if( n%i == 0){ 
+//     isPrimeNumber = false
+//   }
+// }
+// }
+// }
+// console.log(isPrimeNumber(n))
+
+
+// let num = 10
+// let text = num.toString();
+// let reverse = ''
+// for ( i = num.length - 1; i >= 0; i--){
+//   reverse = reverse + num[i]
+// }if(num == reverse){
+//   console.log(true)
+// } else{
+//   console.log(false)
+// }
+
+// let num = '121'
+// let reverse = ''
+// for ( i = num.length - 1; i >= 0; i--){
+//   reverse = reverse + num[i]
+// }if(num == reverse){
+//   console.log(true)
+// } else{
+//   console.log(false)
+// }
+
+// for(let i = 1; i <=5; i++){
+//   let text = '' 
+//   for(let j = 0; j < 5; j++){
+//     if(i > j){
+//       text = text + '*'
+//     } else {
+//       text = text + ''
+//     }
+//   }console.log(text)
+// }
+
+// for(let i = 1; i <= 5; i++){
+//   let text = ''
+//   for(let j=1; j<=5; j++){
+//     if(i >= j){
+//       text = text + '*'
+//     } else{
+//       text = text + ''
+//     }
+//   } console.log(text)
+// }
+
+// for(let i = 5; i >= 1; i--){
+//   let text = ''
+//   for(let j = 5; j >= 1; j--){
+//     if(i >= j){
+//       text = text + '*'
+//     } else{
+//       text = text + ''
+//     }
+//   } console.log(text)
+// }
+// let names = [    "baljan",   
+//    "itgel",    
+//    "jamiyansuren",   
+//    "chingun",    
+//    "telmuun",  
+//    "chinguun",    
+//    "nomi",    
+//    "aygul",    
+//    "behbat",    
+//    "narangerel",    
+//    "oyunbat",    
+//    "turbat",    
+//    "baasandash",    
+//    "jargalsaihan",    
+//    "tugsbileg",    
+//    "huwituguldur",    
+//    "ursaa",    
+//    "ider",    
+//    "ariunjargal" ];
+//   let find ='narangerel';
+
+//   for(let i = 0; i < names.length; i++){ // i = 1
+//       if(find==names[i]){ // "itgel" == "itgel" ==> true
+//           console.log(i)
+//       }
+//   }
+
+//   let names = [    "baljan",   
+//    "itgel",    
+//    "jamiyansuren",   
+//    "chingun",    
+//    "telmuun",  
+//    "chinguun",    
+//    "nomi",    
+//    "aygul",    
+//    "behbat",    
+//    "narangerel",    
+//    "oyunbat",    
+//    "turbat",    
+//    "baasandash",    
+//    "jargalsaihan",    
+//    "tugsbileg",    
+//    "huwituguldur",    
+//    "ursaa",    
+//    "ider",    
+//    "ariunjargal" ];
+//   let find ='narangerel';
+//   for(i = 0; i < names.length; i++){
+//    if(find == names[i]){
+//       console.log(i)
+//    }
+//   }
+
+// let a = 12;
+// let b = 3;
+// let isMax;
+// for (i = 0; i < a; i++) {
+//    if (i == b) {
+//       isMax = a
+//    }
+// }
+// if (!isMax) {
+//    isMax = b
+// }
+// console.log(isMax);
+
+// let a = 12;
+// let b = 14;
+// let diff = ((b - a)**2)**(1/2);
+// let max = (a+b+diff)/2
+// console.log(max)
+
+
+// let a = 4;
+// let b = 5;
+// let c = 6;
+// let diff = ((a-b)**2)**(1/2)
+// let dif = ((a-c)**2)**(1/2)
+// let max = (a+b+c+diff+dif)/3
+// console.log(max)
+
+// let a = 4;
+// let b = 5;
+// let c = 6;
+// if(a >b && a > c){
+//     console.log(a)
+// } else if(b > a && b > c){
+//     console.log(b)
+// } else(console.log(c))
+
+// let a = 2;
+// let b = 4;
+// let c = 5;
+// if(a > b){
+//     if(a > c){
+//     console.log(a)
+//     }
+// } else if(b > a){
+//     if(b > c){
+//     console.log(b)
+//     }
+// } else{console.log(c)}
+
+// let a = 4;
+// let b = 6;
+// let c = 8;
+// let d = 9;
+// if(a > b && a > c){
+//     if(a > d){
+//         if(b > c && b > d){
+//             console.log(b)
+//         }
+//         else if(c > b && c > d){
+//             console.log(c)
+//         }
+//         else{console.log(d)}
+//     }
+//     else{console.log(a)}
+// }
+
+// let a = 3;
+// let b = 4;
+// let c = 5;
+// let d = 6;
+// if(a > b && a > c && a < d){
+//     console.log(a)
+// }
+// else if(a > b && a > d && a < c){
+//     console.log(a)
+// }
+// else if(a > c && a > d && a < b){
+//     console.log(a)
+// }
+// else if(b > a && b > c && b < d){
+//     console.log(b)
+// }
+// else if(b > a && b > d && b < c){
+//     console.log(b)
+// }
+// else if(b > c && b > d && b < a){
+//     console.log(b)
+// }
+// else if(c > a && c > b && c < d){
+//     console.log(c)
+// }
+// else if(c > a && c > d && c < b){
+//     console.log(c)
+// }
+// else if(c > b && c > d && c < a){
+//     console.log(c)
+// }
+// else if(d > a && d > b && d < c){
+//     console.log(d)
+// }
+// else if(d > a && d > c && d < b){
+//     console.log(d)
+// }
+// else if(d > b && d > c && d < a){
+//     console.log(d)
+// }
+
+// var year = 2000;
+// if(year % 4 == 0 && year % 400 == 0 || year % 100 != 0){
+//     console.log(year)
+// }
+
+// let nums = [2, 8, 7, 15]
+// for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//         if (nums[i] == nums[j]) {
+//             console.log(true)
+//         }
+//     }
+// }
+
+// console.log(false)
+
+// let sum = 0;
+// for(let i = 0; i <= 1000; i+=3){
+//  sum += i;
+// } console.log(sum)
+        
+// let sum = 0;
+// for(let i = 0; i <= 1000; i++){
+//     if(i % 3 == 0){
+//         sum += i;
+//     }
+// } console.log(sum)
+
+// function sumOfDigit(num) {
+//     return num.toString().split("")
+//         .reduce((sum, digit) =>
+//             sum + parseInt(digit), 0);
+// }
+// console.log(sumOfDigit(999));
+
+// let a = 1;
+// let b = 11;
+// let sum = (a + b) * (b / a) / 2
+// console.log(sum)
+
+// var sum = 0;
+// for(var i = 0; i <= 1000; i+=3){
+//  sum += i;
+// } console.log(sum)
+
+// let arr = [15, 7, 2, 11]
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = i + 1; j < arr.length; j++){
+//         if(arr[i] > arr[j]){
+//             let temp = arr[i];
+//             arr[i] = arr[j]
+//             arr[j] = temp;
+//         }
+//     }
+// } console.log(arr)
+
+// let arr = [15, 7, 2, 11]
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = 0; j < arr.length; j++){
+//         if(arr[i] > arr[j]){
+//             let temp = arr[i];
+//             arr[i] = arr[j]
+//             arr[j] = temp;
+//         }
+//    console.log(arr)
+//     }
+// } 
+
+// const person = {
+//     name: "Narangerel",
+//     city: "Dalanzadgad",
+//     hobby: "Hiking"
+// }; 
+// console.log(person.name) 
+// console.log(person["age"]) 
+
+// const duck = new Object();
+// duck.name = "Donald";
+// duck.color = "Yellow";
+// duck.size = "Small";
+// console.log(duck.name + " is duck's name.")
+
+// const myObject = {
+//     outerObject: {
+//       innerObject: {
+//         message: "Hello World!"
+//       }
+//     }
+//   };
+//   console.log(myObject.outerObject.innerObject.message); 
+  
+// function printProperties(obj){
+//     console.log(Object.keys(obj))
+// }
+// printProperties({name:"naraa", age:23})
+
+// function printProperties(obj){
+//     console.log(Object.values(obj))
+// }
+// printProperties({name:"naraa", age:23})
+// console.table({name:"naraa", age:23})
+
+// loop, new Date, Date method, condition
+// const date = new Date();
+// console.log(date)
+
+// const newYear = new Date(["2025-12-31", "2026-12-31", "2027-12-31", "2028-12-31", "2029-12-31", "2030-12-31",])
+
+// const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// const d = new Date("2025-12-31");
+// let day = days[d.getDay()];
+// let year = d.getFullYear()
+// let a = 2025;
+// let b = 2030;
+// for(let i = 0; i < (b-a); i++){
+//     year += 1
+//     console.log(year,)
+// }
+
+
+// const days = {'0': 'Ням гараг', 
+//         '1': 'Даваа гараг',   
+//         '2': 'Мягмар гараг',   
+//         '3': 'Лхагва гараг',  
+//         '4': 'Пүрэв гараг',   
+//         '5': 'Баасан гараг',    
+//         '6': 'Бямба гараг',}
+// const reverse = 
+// console.log(Object.entries(days))
+// console.log(Object.entries(days)[1][1])
+
+// const nums = [2, 7, 11, 15];
+// let target = 9;
+
+
+// const weather = [    {        "date": "2024-12-26",        "temperature": {            "current": -18.7,            "min": -22.9,            "max": -14.5        }    },    {        "date": "2024-12-27",        "temperature": {            "current": -19.2,            "min": -24.0,            "max": -14.4        }    },    {        "date": "2024-12-28",        "temperature": {            "current": -17.4,            "min": -21.2,            "max": -13.5        }    },    {        "date": "2024-12-29",        "temperature": {            "current": -16.4,            "min": -22.0,            "max": -10.7        }    },    {        "date": "2024-12-30",        "temperature": {            "current": -15.7,            "min": -20.5,            "max": -10.9        }    },    {        "date": "2024-12-31",        "temperature": {            "current": -17.4,            "min": -22.7,            "max": -12.1        }    },    {        "date": "2025-01-01",        "temperature": {            "current": -20.4,            "min": -23.9,            "max": -16.8        }    },    {        "date": "2025-01-02",        "temperature": {            "current": -20.1,            "min": -25.8,            "max": -14.3        }    },    {        "date": "2025-01-03",        "temperature": {            "current": -17.8,            "min": -23.5,            "max": -12.0        }    },    {        "date": "2025-01-04",        "temperature": {            "current": -18.3,            "min": -22.4,            "max": -14.2        }    },    {        "date": "2025-01-05",        "temperature": {            "current": -16.8,            "min": -22.0,            "max": -11.6        }    },    {        "date": "2025-01-06",        "temperature": {            "current": -16.6,            "min": -22.4,            "max": -10.7        }    },    {        "date": "2025-01-07",        "temperature": {            "current": -15.9,            "min": -22.2,            "max": -9.6        }    },    {        "date": "2025-01-08",        "temperature": {            "current": -15.6,            "min": -21.9,            "max": -9.3        }    }]
+
+// const nums = [1, 2, 3, 2]
+// for(let i = 0; i < nums.length; i++) {
+//     for(let j = i + 1; j < nums.length; j++){
+//         if(nums[i] == nums[j]){
+//          return true
+//         }
+
+//     }
+// }
+// return false
+
+
+
+
